167d44bf182ddab7ee468efc999fa82ebf9b60d7
wellplayed <daffanfproject@gmail.com>
2023-02-11 20:04:03 +0700

    Finish Final Class

---
+++ src/daffanaufal/data/SocialMedia.java
@@ -0,0 +1,14 @@
+package daffanaufal.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+// Error
+//class FakeFacebook extends Facebook {
+//
+//}
